ff6cc0da8936aa48dcd7371201bb0fa7abb5591b
Jonny Mortemore <jonathanmortemore@gmail.com>
2025-06-17 14:06:33 +1000

    Basic form functionality to add a new book

--- scripts.js
+++ scripts.js
@@ -1,5 +1,6 @@
 const myLibrary = [];
-addBookToLibrary();
+addBookToLibrary("The Fellowship of the Ring", "J.R.R Tolkien", "1000");
+addBookToLibrary("The Expanse", " James S. A. Corey", "800");
 displayBooks();
 
 function Book(title, author, pages) {
@@ -19,23 +20,31 @@ function Book(title, author, pages) {
     }
 }
 
+document.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const form = new FormData(event.target);
+    addBookToLibrary(form.get("book-name"), form.get("author"), form.get("pages"));
+    displayBooks();
+});
 
 
-function addBookToLibrary() {
-    myLibrary.push(new Book("The Fellowship of the Ring", "J.R.R Tolkien", "1000"));
-    myLibrary.push(new Book("The Expanse", " James S. A. Corey", "800"));
+function addBookToLibrary(title, author, pages) {
+    myLibrary.push(new Book(title, author, pages));
 }
 
 
 
 function displayBooks() {
     const bookDisplay = document.querySelector('tbody');
+    document.querySelectorAll(".bookRow").forEach((el) => {
+        el.remove();
+    })
     myLibrary.forEach((e) => {
         const bookRow = document.createElement('tr');
         const bookName = document.createElement('td');
         const bookAuthor = document.createElement('td');
         const bookPages = document.createElement('td');
-
+        bookRow.className = "bookRow";
         bookName.innerText = e.title;
         bookAuthor.innerText = e.author;
         bookPages.innerText = e.pages;
